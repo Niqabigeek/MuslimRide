96bd95f9fd237c0db1bab541bcc087100d264d70
Zeniat Mohammed <zeniatmohammed10@gmail.com>
2023-01-11 23:08:06 +0100

    muslimRide commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// document.getElementById('count-el').innerText = 45
+
+// let count= 8
+
+// console.log(count)
+
+
+// let myAge= 26
+
+// console.log(myAge)
+
+// let myAge= 26
+// let humanDogRatio= 54
+
+// let myDogAge = myAge + humanDogRatio
+
+// console.log(myDogAge)
+
+let saveEl = documemt.getElementById('save-btn')
+let countEl = document.getElementById('count-btn')
+let count = 0
+
+function increment(){
+
+    count = count + 1
+    countEl.innerText = count
+    console.log(countEl)
+}
+
+let username = "zenny"
+let message = 'you have three new notification'
+
+console.log(message + "," + username + "!")
+
